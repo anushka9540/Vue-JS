ef6bfc4be4bbea4e46b75d88cfc938786512a688
Anushka Prajapati <anushkaprajapati827@gmail.com>
2025-02-28 10:30:42 +0530

    implemented reaction game

--- src/components/GameModal.vue
+++ src/components/GameModal.vue
@@ -1,14 +1,14 @@
 <template>
-    <div v-if="show" class="modal">
-        <div class="modal-content">
-            <h3>Reaction Time Result</h3>
-            {{ result }}
-            <p v-if="highScore">
-                High Score: <strong>{{ highScore }}s</strong>
-            </p>
-            <button v-on:click="$emit('close')">OK</button>
-        </div>
+  <div v-if="show" class="modal">
+    <div class="modal-content">
+      <h3>Reaction Time Result</h3>
+      {{ result }}
+      <p v-if="highScore">
+        High Score: <strong>{{ highScore }}s</strong>
+      </p>
+      <button v-on:click="$emit('close')">OK</button>
     </div>
+  </div>
 </template>
   
   <script>
@@ -33,17 +33,17 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-}
-
-.modal-content {
+  }
+  
+  .modal-content {
     background: white;
     padding: 20px;
     border-radius: 10px;
     text-align: center;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-}
-
-.modal-content button {
+  }
+  
+  .modal-content button {
     margin-top: 10px;
     padding: 10px 20px;
     border: none;
@@ -51,5 +51,5 @@ export default {
     color: white;
     border-radius: 5px;
     cursor: pointer;
-}
+  }
 </style>
